eb2256330a0603429f3ec25612bdb2b19b2dcfdf
DavidWallJ <djkindread@gmail.com>
2017-05-27 09:58:41 +0800

    wired up controls component

--- app/components/Controls.jsx
+++ app/components/Controls.jsx
@@ -5,22 +5,29 @@ import React, { Component } from 'react';
 
 const Controls  = React.createClass({
   propTypes: {
-    countdownStatus: React.PropTypes.string.isRequired
+    countdownStatus: React.PropTypes.string.isRequired,
+    onStatusChange: React.PropTypes.func.isRequired
   },
+  onStatusChange: function (newStatus) {
+    return () => {
+      this.props.onStatusChange(newStatus)
+    }
+  },
+  // onStatusChange is a function that returns a function.  the onClick handlers are a bit different here.  They pass-on the argument value.  The argument value is passed to 'newStatus'.  'newStatus' is saved on the props object.  This is called the currying pattern.
   render: function () {
     const {countdownStatus} = this.props;
     const renderStartStopButton = () => {
       if (countdownStatus === 'started') {
-        return <button className="button secondary">Pause</button>
+        return <button className="button secondary" onClick={this.onStatusChange('paused')}>Pause</button>
       } else if (countdownStatus === 'paused') {
-        return <button className="button primary">Start</button>
+        return <button className="button primary" onClick={this.onStatusChange('started')}>Start</button>
       }
     };
 
     return (
       <div className="controls">
         {renderStartStopButton()}
-        <button className="button alert hollow">Clear</button>
+        <button className="button alert hollow" onClick={this.onStatusChange('stopped')}>Clear</button>
       </div>
     )
 
--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Clock from 'Clock';
 import CountdownForm from 'CountdownForm';
+import Contorls from 'Controls';
 
 const Countdown = React.createClass({
   getInitialState: function () {
@@ -15,6 +16,13 @@ const Countdown = React.createClass({
         case 'started':
           this.startTimer();
           break;
+        case 'stopped':
+          this.setState({count: 0});
+          // no break here.  we want it to follow through to the next case as well
+        case 'paused':
+          clearInterval(this.timer)
+          this.timer = undefined;
+          break
       }
     }
   },
@@ -33,12 +41,23 @@ const Countdown = React.createClass({
       countdownStatus: 'started'
     });
   },
+  handleStatusChange: function (newStatus) {
+    this.setState({countdownStatus: newStatus})
+  },
   render: function () {
-    const {count} = this.state;
+    const {count, countdownStatus} = this.state;
+    const renderControlArea = () => {
+      if (countdownStatus !== 'stopped') {
+        return <Contorls countdownStatus={countdownStatus} onStatusChange={this.handleStatusChange}/>
+      } else {
+        return <CountdownForm onSetCountdown={this.handleSetCountdown}/>;
+      }
+    };
+
     return (
       <div>
         <Clock totalSeconds={count}/>
-        <CountdownForm onSetCountdown={this.handleSetCountdown} />
+        {renderControlArea()}
       </div>
     )
   }
